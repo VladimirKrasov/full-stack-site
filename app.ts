f6bc6d4ee15ed09a24bf2b258641bd439ced9404
VladimirKrasov <krasovvcc@gmail.com>
2021-01-17 12:33:53 +0300

    init data base

--- app.ts
+++ app.ts
@@ -1,8 +1,23 @@
 const exprees = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = exprees()
 
 const PORT = config.get('port')
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+const  start = async() => {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+  } catch (e) {
+    console.log('Server Error:', e.message)
+    process.exit(1)
+  }
+}
+
+start()
