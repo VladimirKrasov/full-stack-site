8573f021a0b4f5ecf4a8ae5e39432147b2202212
VladimirKrasov <krasovvcc@gmail.com>
2021-01-17 11:30:55 +0300

    init server

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log('Runn server')
+const exprees = require('express')
+const config = require('config')
+
+const app = exprees()
+
+const PORT = config.get('port')
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
